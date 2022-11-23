b155ecdf825d15801fa6fdf7b4feef28e5b84afc
shubhjadhav <shubhamjadhav1017@gmail.com>
2022-11-22 21:40:56 -0500

    Adding time calculations to previous commit

--- Fall 2022/CSCI_6212/Projects/Project 3/Project3.py
+++ Fall 2022/CSCI_6212/Projects/Project 3/Project3.py
@@ -1,28 +1,40 @@
 # A Dynamic Programming based Python Program for the Egg Dropping Puzzle
+
+# Import libraries for capturing time and math functions
+import math
+import time
+import matplotlib.pyplot as plt
+import functools
 import sys
+
+# Initialize list to capture results for theoretical, experimental results and values of n
+theoreticalTimeList = []
+experimentalTimeList = []
+nValuesList = []
 
 # Function to get minimum number of trials needed in worst
 # case with n eggs and k floors
-def eggDrop(n, k):
+def eggDrop(m, n):
+
 	# A 2D table where entry eggFloor[i][j] will represent minimum
 	# number of trials needed for i eggs and j floors.
-    eggFloor = [[0]*(k+1) for i in range(n+1)]
+    eggFloor = [[0]*(n+1) for i in range(m+1)]
     first_floor_value = sys.maxsize
     first_floor = sys.maxsize
 
 	# We need one trial for one floor and0 trials for 0 floors
-    for i in range(1, n + 1):
+    for i in range(1, m + 1):
         eggFloor[i][1] = 1
         eggFloor[i][0] = 0
 
 	# We always need j trials for one egg and j floors.
-    for j in range(1, k + 1):
+    for j in range(1, n + 1):
         eggFloor[1][j] = j
 
 	# Fill rest of the entries in table using optimal substructure
 	# property
-    for i in range(2, n + 1):
-        for j in range(2, k + 1):
+    for i in range(2, m + 1):
+        for j in range(2, n + 1):
             eggFloor[i][j] = sys.maxsize
             res = sys.maxsize
 
@@ -38,21 +50,88 @@ def eggDrop(n, k):
             
 
 	# eggFloor[n][k] holds the result
-    return first_floor, first_floor_value
+    return eggFloor[m][n]
 
-# Driver program to test to print printDups
-n = 1
-k = 36
-# print(
-#     "Minimum number of trials in worst case with " 
-#     + str(n) 
-#     + " eggs and "
-# 	+ str(k) 
-#     + " floors is " 
-#     + str(eggDrop(n, k))
-# )
+def calculateTime(eggs, floors, var):
 
-first_floor, first_floor_value = eggDrop(n, k)
+    # Capture the time, when the function's execution has begun
+    startTime = time.time()
 
-print(first_floor, first_floor_value)
+    # The algorithm to find staircase
+    eggDrop(eggs, floors)
 
+
+    # Capture the time, when the function's execution has ended
+    endTime = time.time()
+
+    # Calculate the time taken to execute the code snippet
+    timeTaken = endTime-startTime
+
+    # Append the values of n, experimental results and theoretical results in their corresponding lists
+
+    nValuesList.append(var)
+    experimentalTimeList.append(timeTaken)
+    theoreticalTimeList.append((var)*math.log2(var)) # Theoretical results derived
+
+# Having number of floors constant
+floors = 1000
+eggs = [2,4,6,8,10]
+
+for num_of_egg in eggs:
+    calculateTime(num_of_egg,floors, num_of_egg)
+
+# Function to calculate mean of the results which will be used for normalization
+def Average(lst):
+    return functools.reduce(lambda val1, val2: val1 + val2, lst) / len(lst)
+
+# Deriving scaling/normalizing constant by mean of theoretical results by experimental results
+normalizingConstant = Average(theoreticalTimeList)/Average(experimentalTimeList)
+
+
+print("\n\n-------------------------------------------------------------------------------------------------")
+print("-------------------------------For variable eggs and fixed floors--------------------------------")
+print("-------------------------------------------------------------------------------------------------\n\n")
+
+print("\nThe number of floors is {}\n".format(floors))
+
+print("\nThe normalizing Constant fixed number of floors is = {}".format(normalizingConstant))
+
+# Creating a lnew ist of experimental results by scaling the existing with normalizing constant
+adjustedexperimentalTimeList = [i * normalizingConstant for i in experimentalTimeList]
+
+# Logs of values of n, Time taken Programatically and Theoretically which is derived in the project submission
+for i,n in enumerate(eggs):
+    print("\nFor n={}".format(n))
+    print("Theoretically Time taken = {}".format(theoreticalTimeList[i]))  # Theoretical results derived
+    print("Programatically Time taken = {}".format(experimentalTimeList[i]))
+    print("Adjusted Programatically Time taken = {}".format(adjustedexperimentalTimeList[i]))
+
+
+
+floors = [10, 100, 500, 1000, 5000]
+eggs = 2
+
+for num_of_floor in floors:
+    calculateTime(eggs,num_of_floor, num_of_floor)
+
+# Deriving scaling/normalizing constant by mean of theoretical results by experimental results
+normalizingConstant = Average(theoreticalTimeList)/Average(experimentalTimeList)
+
+print("\n\n-------------------------------------------------------------------------------------------------")
+print("-------------------------------For variable floors and fixed eggs--------------------------------")
+print("-------------------------------------------------------------------------------------------------\n\n")
+
+print("\nThe number of eggs is {}\n".format(eggs))
+
+print("\nThe normalizing Constant for fixed number of eggs is = {}".format(normalizingConstant))
+
+# Creating a lnew ist of experimental results by scaling the existing with normalizing constant
+adjustedexperimentalTimeList = [i * normalizingConstant for i in experimentalTimeList]
+
+
+# Logs of values of n, Time taken Programatically and Theoretically which is derived in the project submission
+for i,n in enumerate(floors):
+    print("\nFor n={}".format(n))
+    print("Theoretically Time taken = {}".format(theoreticalTimeList[i]))  # Theoretical results derived
+    print("Programatically Time taken = {}".format(experimentalTimeList[i]))
+    print("Adjusted Programatically Time taken = {}".format(adjustedexperimentalTimeList[i]))
